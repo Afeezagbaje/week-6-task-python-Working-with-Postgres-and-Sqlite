4225a71a732107d7d469101264e7d53ce0d584dd
Afeez Agbaje <afeezagbaje@gmail.com>
2021-05-04 15:24:51 +0100

    added a file that can add csv files to the SQlite database

--- csv_to_database.py
+++ csv_to_database.py
@@ -4,31 +4,68 @@ import csv
 
 class CsvToDatabase:
     def __init__(self):
-        pass
-    
-    def create_table(self):
-        pass
-        
-    def load_data(self):
-        pass
-    
-    def all_student_result(self):
-        pass
+        self.connection = sqlite3.connect('gradedb.sqlite')
+        self.cursor = self.connection.cursor()
 
-    def add_student(self):
-        pass
-    
-    def update(self):
-        pass
-        
+    def create_table(self):
+        self.cursor.execute('DROP TABLE IF EXISTS grades')
+        self.cursor.execute("""CREATE TABLE IF NOT EXISTS grades(Last_name TEXT NOT NULL,
+                            First_name TEXT NOT NULL, SSN TEXT PRIMARY KEY, Test1 INTEGER NOT NULL,
+                            Test2 INTEGER NOT NULL, Test3 INTEGER NOT NULL, Test4 INTEGER NOT NULL,
+                            Final INTEGER NOT NULL, Grade TEXT NOT NULL);""")
+        self.connection.commit()
+
+    def load_data(self):
+        with open('grades.csv') as file:
+            file_reader = csv.reader(file)
+            csv_data = list(file_reader)
+            data_body = csv_data[1:]
+        for row in data_body:
+            row_tuple = tuple(row)
+            self.cursor.execute('INSERT INTO grades VALUES (?,?,?,?,?,?,?,?,?)', row_tuple)
+        self.connection.commit()
+
+    def all_student_result(self):
+        self.cursor.execute('SELECT * FROM grades')
+        return self.cursor.fetchall()
+
+    def add_student(self, last_name, first_name, ssn, test1, test2, test3, test4, final, grade):
+        try:
+            insert_into_database = 'INSERT INTO grades VALUES (?,?,?,?,?,?,?,?,?)'
+            insert_values = (last_name, first_name, ssn, test1, test2, test3, test4, final, grade)
+            self.cursor.execute(insert_into_database, insert_values)
+            self.connection.commit()
+            return self.all_student_result()
+
+        except NameError:
+            return 'Information not valid. Please try again'
+
+    def update(self, test1, test2, test3, test4, final, grade, ssn):
+        update_query = 'UPDATE grades SET test1=?, test2=?, test3=?, test4=?, final=?, grade=? WHERE ssn=?'
+        data = (test1, test2, test3, test4, final, grade, ssn)
+        self.cursor.execute(update_query, data)
+        self.connection.commit()
+        return self.all_student_result()
+
     def passed(self):
-        pass
-    
+        self.cursor.execute('SELECT * FROM grades WHERE final>="50"')
+        return self.cursor.fetchall()
+
     def passed_test1(self):
-        pass
-    
+        self.cursor.execute('SELECT * FROM grades WHERE test1>="45"')
+        return self.cursor.fetchall()
+
     def failed(self):
-        pass
-    
-    def delete(self):
-        pass
+        self.cursor.execute('SELECT * FROM grades WHERE final<"50"')
+        return self.cursor.fetchall()
+
+    def delete(self, ssn):
+        delete_query = 'DELETE FROM grades WHERE ssn=?'
+        data = (ssn,)
+        self.cursor.execute(delete_query, data)
+        self.connection.commit()
+        return self.all_student_result()
+
+
+cs = CsvToDatabase()
+print(cs.all_student_result())
--- tests/test_csv_to_database.py
+++ tests/test_csv_to_database.py
@@ -5,40 +5,46 @@ from ..csv_to_database import CsvToDatabase
 class TestCsvToDatabase(unittest.TestCase):
     def setUp(self):
         self.execute = CsvToDatabase()
+        self.execute.create_table()
         self.execute.load_data()
-        
+
     def test_all_student_result(self):
         self.assertIsNotNone(self.execute.all_student_result())
         self.assertIsInstance(self.execute.all_student_result(), list)
-        
+
     def test_add_student(self):
-        self.assertIsNotNone(self.execute.add_student())
-        self.assertIsInstance(self.execute.add_student, list)
-    
+        before_adding_student = len(self.execute.all_student_result())
+        add_student = self.execute.add_student('Agbaje', 'Afeez', '419-41-9419', '100', '90', '80', '85', '85', 'A')
+        after_adding_student = len(self.execute.all_student_result())
+        self.assertNotEqual(before_adding_student, after_adding_student)
+        self.assertIsNotNone(add_student)
+
     def test_update(self):
-        self.assertIsNotNone(self.execute.update())
-        self.assertIsInstance(self.execute.update(), list)
-    
+        update = self.execute.update('100', '90', '80', '85', '85', 'A', '419-41-9419')
+        self.assertIsNotNone(update)
+        self.assertIsInstance(update, list)
+
     def test_passed(self):
         self.assertIsNotNone(self.execute.passed())
         self.assertIsInstance(self.execute.passed(), list)
-    
+
     def test_passed_test1(self):
         self.assertIsNotNone(self.execute.passed_test1())
         self.assertIsInstance(self.execute.passed_test1(), list)
-    
+
     def test_failed(self):
         self.assertIsNotNone(self.execute.failed())
         self.assertIsInstance(self.execute.failed(), list)
-    
+
     def delete(self):
-        self.assertIsNotNone(self.execute.delete())
-        self.assertIsInstance(self.execute.delete(), list)
-        
+        delete_student = self.execute.delete('419-41-9419')
+        self.assertIsNotNone(delete_student)
+        self.assertIsInstance(self.execute.delete(delete_student), list)
+
     def tearDown(self):
         self.execute.cursor.close()
         self.execute.connection.close()
 
-        
+
 if __name__ == '__main__':
     unittest.main()
